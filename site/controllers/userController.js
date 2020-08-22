63c574b59c8c539d6db660769277f9e24c1d30e5
MaxiDeMichieli <maxidemichieli@gmail.com>
2020-08-22 19:09:48 -0300

    agrego: funcionalidad crear usuario, vista datos de usuario, vista y funcionalidad editar datos de usuario, cambio boton de usurio en el header, base de datos de usuarios

--- site/controllers/userController.js
+++ site/controllers/userController.js
@@ -1,5 +1,67 @@
-module.exports = function(req, res, next) {
-    res.render('register', {
-        title: 'Formulario de registro'
-    });
-  }
+const dbUsers = require('../data/usersDatabase');
+const fs = require('fs');
+const path = require('path')
+
+module.exports = {
+    registerView: (req, res) => {
+        res.render('register', {
+            title: 'Formulario de registro'
+        });
+    },
+    register: (req, res, next) => {
+        let newUser = {
+            id: dbUsers.length + 1,
+            nombre: req.body.nombre.trim(),
+            apellido: req.body.apellido.trim(),
+            email: req.body.email.trim(),
+            password: req.body.password,
+            telefono: null,
+            calle: null,
+            numero: null,
+            dpto: null,
+            cpostal: null,
+            provincia: null,
+            localidad: null,
+        }
+
+        dbUsers.push(newUser);
+
+        fs.writeFileSync(path.join(__dirname, '..', 'data', 'usersDataBase.json'), JSON.stringify(dbUsers), 'utf-8');
+
+        res.redirect('/users/account')
+    },
+    account: (req, res) => {
+        res.render('account', {
+            title: 'Mi cuenta',
+            user: dbUsers[dbUsers.length - 1]
+        })
+    },
+    editView: (req, res) => {
+        res.render('editAccount', {
+            title: 'Editar mis datos',
+            user: dbUsers[dbUsers.length - 1]
+        })
+    },
+    edit: (req, res) => {
+        let idUser = req.body.id;
+        dbUsers.forEach(user => {
+            if (user.id == idUser) {
+                user.id = Number(req.body.id);
+                user.nombre = req.body.nombre.trim();
+                user.apellido = req.body.apellido.trim();
+                user.email = req.body.email.trim();
+                user.telefono = req.body.telefono.trim();
+                user.calle = req.body.calle.trim();
+                user.numero = req.body.numero.trim();
+                user.dpto = req.body.dpto.trim();
+                user.cpostal = req.body.cpostal.trim();
+                user.provincia = req.body.provincia.trim();
+                user.localidad = req.body.localidad.trim();
+            }
+        })
+
+        fs.writeFileSync(path.join(__dirname, '../data/usersDataBase.json'), JSON.stringify(dbUsers))
+        res.redirect('/users/account')
+    }
+}
+
--- site/routes/users.js
+++ site/routes/users.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const userController = require('../controllers/userController')
+const controller = require('../controllers/userController')
 
 /* GET users listing. */
-router.get('/', userController);
+router.get('/account', controller.account);
+
+router.get('/account/edit', controller.editView);
+router.post('/account/edit', controller.edit)
+
+router.get('/login', controller.registerView);
+router.post('/login/create', controller.register)
 
 module.exports = router;
