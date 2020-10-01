4e67fef530686aef0aabbdaef6aef4b73522c471
MaxiDeMichieli <maxidemichieli@gmail.com>
2020-10-01 00:44:46 -0300

    arregla error en el adminUserCheck al entrar a agregar productos

--- site/src/middlewares/adminUserCheck.js
+++ site/src/middlewares/adminUserCheck.js
@@ -1,13 +1,19 @@
 const dbUsers = require('../data/usersDataBase');
 
 module.exports = (req, res, next) => {
+    let admin = false;
+
     dbUsers.forEach(user => {
         if(req.session.usuario.id == user.id){
             if(user.admin){
-                next();
+                admin = true;
             }
         }
     })
 
-    res.redirect('/')
+    if(admin){
+        next();
+    }else{
+        res.redirect('/')
+    }
 }
